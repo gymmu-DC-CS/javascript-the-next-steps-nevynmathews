ee813566e0e54c6c7b3ba67a3dea33594a894f0d
buyarsnasehoor <boye.lffel@gmail.com>
2023-11-21 16:04:25 +0100

    Remove e and E

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+   if (currentElement != "e" && currentElement !="E")
     result.push(currentElement)
   }
   return result.join("")
 }
+
+
