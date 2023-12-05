3b4170a50545b2806c96c38155cd98b59878f80e
buyarsnasehoor <boye.lffel@gmail.com>
2023-12-05 15:17:55 +0100

    commit 2

--- script.js
+++ script.js
@@ -6,8 +6,33 @@ export function exercise01(args) {
     const currentElement = input[i]
    if (currentElement != "e" && currentElement !="E")
     result.push(currentElement)
+
+   
   }
+
   return result.join("")
 }
 
 
+
+export function exercise02(args) {
+  const input = args
+  const result = []
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+ 
+    const capitalizedElement = currentElement.toUpperCase()
+    result.push(capitalizedElement)
+}
+return result.join("")
+}
+
+export function exercise03(args) {
+const input = args
+const result = []
+
+
+
+
+}
