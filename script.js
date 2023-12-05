88b96ae9ff24a29990399b24be1fa1dcb8bfb88b
buyarsnasehoor <boye.lffel@gmail.com>
2023-12-05 16:03:38 +0100

    commit 3

--- script.js
+++ script.js
@@ -29,10 +29,22 @@ return result.join("")
 }
 
 export function exercise03(args) {
-const input = args
-const result = []
-
-
-
-
+  const input = args
+  const result = []
+  let count = 0
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+if (currentElement === 'e'){
+  count = count + 1
 }
+else if (currentElement === 'E'){
+  count = count + 1
+}
+  }
+  return count
+}
+
+export function exercise04(args) {
+  
+
