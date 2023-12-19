f0e6498d42d20af208640783abd597c5f74aa8c2
buyarsnasehoor <boye.lffel@gmail.com>
2023-12-19 15:43:34 +0100

    exercise06

--- script.js
+++ script.js
@@ -4,102 +4,91 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-   if (currentElement != "e" && currentElement !="E")
-    result.push(currentElement)
-
-   
+    if (currentElement != "e" && currentElement != "E")
+      result.push(currentElement)
   }
 
   return result.join("")
 }
 
-
-
 export function exercise02(args) {
   const input = args
   const result = []
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
- 
+
     const capitalizedElement = currentElement.toUpperCase()
     result.push(capitalizedElement)
-}
-return result.join("")
+  }
+  return result.join("")
 }
 
 export function exercise03(args) {
   const input = args
   const result = []
   let count = 0
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-if (currentElement === 'e'){
-  count = count + 1
-}
-else if (currentElement === 'E'){
-  count = count + 1
-}
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
   }
   return count
 }
 
-export function exercise04 (args) {
+export function exercise04(args) {
   const input = args
   const result = []
   print(input)
- 
+
   let count = 0
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-   
+
     if ("+,-.#%&/()=?£}{$¦@¬|¢´!<>°§;':_~^*][¨".indexOf(currentElement) >= 0) {
-      
-    }  else{
-        result.push(currentElement)
-}
-}
- 
-const clearedString = result.join("")
+    } else {
+      result.push(currentElement)
+    }
+  }
 
-const tmp = clearedString.split(" ")
+  const clearedString = result.join("")
 
-print(tmp)
- 
-return tmp.length
-}
+  const tmp = clearedString.split(" ")
 
-export function exercise05 (args) {
-const input = args
+  print(tmp)
 
-for (let i = 0; i < input.length; i++) {
-const currentElement = input[i].charCodeAt()
-if (currentElement >= 65 && currentElement <= 90)
-return true
+  return tmp.length
 }
 
-return false
-
-}
-
-export function exercise06 (args) {
+export function exercise05(args) {
   const input = args
-  
+
   for (let i = 0; i < input.length; i++) {
-  const currentElement = input[i]
-  if (
-( )
-
-
-
-  )
-  return true
+    const currentElement = input[i].charCodeAt()
+    if (currentElement >= 65 && currentElement <= 90) return true
   }
-  
+
   return false
-  
-  }
-  
-  
+}
+
+export function exercise06(args) {
+  const input = args
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (
+      (currentElement >= 33 && currentElement <= 47) ||
+      (currentElement >= 58 && currentElement <= 64) ||
+      (currentElement >= 91 && currentElement <= 96) ||
+      ( currentElement >= 123 )
+    ) {
+      return true
+    }
+    }
+      return false
+}
